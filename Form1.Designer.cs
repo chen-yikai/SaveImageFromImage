2ca39dd46f522e43f8fdf6c1bf231642f111d089
chen_yikai <s11304127@school.saihs.edu.tw>
2024-12-16 20:39:06 +0800

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace SaveImageFromImage
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.saveImage = new System.Windows.Forms.Button();
+            this.NewFileName = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(131, 431);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(181, 70);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Load a File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.loadImage_click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(131, 28);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(385, 287);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // saveImage
+            // 
+            this.saveImage.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.saveImage.Location = new System.Drawing.Point(335, 431);
+            this.saveImage.Name = "saveImage";
+            this.saveImage.Size = new System.Drawing.Size(181, 70);
+            this.saveImage.TabIndex = 2;
+            this.saveImage.Text = "Save Image";
+            this.saveImage.UseVisualStyleBackColor = true;
+            this.saveImage.Click += new System.EventHandler(this.saveImage_Click);
+            // 
+            // NewFileName
+            // 
+            this.NewFileName.Location = new System.Drawing.Point(260, 368);
+            this.NewFileName.Name = "NewFileName";
+            this.NewFileName.Size = new System.Drawing.Size(114, 20);
+            this.NewFileName.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(683, 567);
+            this.Controls.Add(this.NewFileName);
+            this.Controls.Add(this.saveImage);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button saveImage;
+        private System.Windows.Forms.TextBox NewFileName;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SaveImageFromImage
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void loadImage_click(object sender, EventArgs e)
+        {
+            string imagePath = @"C:\Users\elias\Desktop\custom\eliaschen.jpg";
+
+            Image image = Image.FromFile(imagePath);
+            pictureBox1.Image = image;
+        }
+
+        private void saveImage_Click(object sender, EventArgs e)
+        {
+            string imagePath = @"C:\Users\elias\Desktop\custom\eliaschen.jpg";
+            string saveTarget = @"C:\Users\elias\Desktop\custom";
+
+
+            Image image = Image.FromFile(imagePath);
+            image.Save(System.IO.Path.Combine(saveTarget, NewFileName.Text + ".png"), System.Drawing.Imaging.ImageFormat.Png);
+            image.Dispose();
+        }
+    }
+}
